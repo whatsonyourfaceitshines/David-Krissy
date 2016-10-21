c014b1ff398a65b8ab1ba3c60200e02a7b2538ed
Krissy <Krissroxs@hotmail.com>
2016-10-21 09:45:19 -0600

    10/21/16 BattleControl functions

--- Gladiator/src/byui/cit260/gladiator/control/BattleControl.java
+++ Gladiator/src/byui/cit260/gladiator/control/BattleControl.java
@@ -10,9 +10,16 @@ package byui.cit260.gladiator.control;
  * @author Krissy
  */
 public class BattleControl {
-    public int characterHP (int armor){
-        return = characterHP;}
-    public int charaterAT (int weapon){}
-    public int damageToEnemy (int characterAT){}
-    public int damageToCharacter (int enemyAT){}
+    public int characterHP (int armor, int baseHP){
+        int health = armor + baseHP;
+        return health;}
+    public int charaterAT (int weapon, int baseAT){
+        int attack = weapon + baseAT;
+        return attack;}
+    public int damageToEnemy (int characterAT, int weapon, int enemyArmor){
+        int damage = characterAT + weapon - enemyArmor;
+        return damage;}
+    public int damageToCharacter (int enemyAT, int armor, int health){
+        int characterDamage = enemyAT - armor;
+        return characterDamage;}
 }
