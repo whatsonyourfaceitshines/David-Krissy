8816e68919a714363a751b7ea8a07630c78c9a73
David <David@David-PC>
2016-10-31 00:01:41 -0600

    10/31/2016 - David did more work on the View layer.

--- Gladiator/src/byui/cit260/gladiator/view/StartProgramView.java
+++ Gladiator/src/byui/cit260/gladiator/view/StartProgramView.java
@@ -5,6 +5,8 @@
  */
 package byui.cit260.gladiator.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author David
@@ -20,11 +22,57 @@ public class StartProgramView {
     }
 
     public void displayStartProgramView() {
-        this.promptMessage = "\nPlease enter your name: ";
-    
-        this.displayBanner();
+       /* displayView(): void 
+        BEGIN 
+        do  
+         Prompt for and get the input value 
+         if (value == “Q”) then 
+          return  
+         do requested action and display the next view 
+       while the view is not done
+        END */
+       
+       this.promptMessage = "\nPlease enter your name: ";
+       
+       boolean done = false;
+       do {
+           this.displayBanner();
+           
+           String playersName = this.getPlayersName();
+           if (playersName.toUpperCase().equals("Q"))
+               return;
+           
+           done = this.doAction(playersName);
+           
+       } while (!done);
+    }
+
+    private String getPlayersName() {
         
-        System.out.println("\nIt works, yo!");
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+        String value = ""; // value to be returned
+        boolean valid = false; // initialize to not valid
+        
+        while (!valid) { // loop while an invalid value is entered
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+            
+            if (value.length() < 1) { // value is blank
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            
+            break; // end the loop
+        }
+        
+        return value; // return the value entered
+    }
+
+    private boolean doAction(String playersName) {
+        System.out.println("\n*** doAction() called ***");
+        return true;
     }
     
 }
